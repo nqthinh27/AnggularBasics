7e10b8539da14542b4bc5bb815789de79ef4b700
NQT <thinhnb2712@gmail.com>
2023-06-20 00:31:49 +0700

    notify and bootstrap

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div class = "alert alert-success"><p>This website uses cookies to provide better user experience</p></div>',
+  styles: ['p {font-weight: bold; font-size: 20px}']
+})
+export class NotificationComponent {
+
+}
